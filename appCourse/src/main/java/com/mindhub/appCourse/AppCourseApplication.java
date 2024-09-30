b46ef7fe47127c880a652226cc895ac708250959
Mariano Avilés <97409207+Maviles94@users.noreply.github.com>
2024-09-29 21:38:05 -0300

    first commit

---
+++ appCourse/src/main/java/com/mindhub/appCourse/AppCourseApplication.java
@@ -0,0 +1,13 @@
+package com.mindhub.appCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCourseApplication.class, args);
+	}
+
+}
---
+++ appCourse/src/test/java/com/mindhub/appCourse/AppCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindhub.appCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
